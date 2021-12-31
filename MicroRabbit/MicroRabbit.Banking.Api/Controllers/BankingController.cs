652fb6776ca754bbb2dc4c03e980d50839303fdb
ahmedraja26 <ahmedahmedraja@gmail.com>
2021-12-31 11:12:44 +0200

    Added Mvc to do transfer with microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MicroRabbit.Banking.Api.Controllers
 {
 	[ApiController]
-	[Route("[controller]")]
+	[Route("api/[controller]")]
 	public class BankingController : ControllerBase
 	{
 
